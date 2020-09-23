8a26600edab7a6856c204db216e010a32614afe6
Nicholas Milson <nickym@Nicholass-MacBook-Air-2.local>
2020-09-23 02:39:59 -0300

    First commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+
+setup(
+    name="BinaryStarSolver",
+    version="0.0.5",
+    author="Nicholas Milson",
+    author_email="nick.milson@dal.ca",
+    description="Solves for the orbital elements of binary stars, given radial velocity time series",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NickMilsonPhysics/binarystarsolve",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
